432a41a8a11efa9d49fca9199b1a7e8078fa5c71
phuongdongiot <phuongdongiot@gmail.com>
2026-05-05 10:45:01 +0700

    feat: update site logo, standardize image paths, and add theme header component

--- fenica-theme/header.php
+++ fenica-theme/header.php
@@ -19,49 +19,49 @@
     </div>
 
     <!-- Preloader -->
-    <?php if ( is_front_page() || is_page_template('front-page.php') ) : ?>
-    <div id="preloader"
-        class="fixed inset-0 z-[9999] bg-[#0e1e2e] flex flex-col items-center justify-center px-6 overflow-hidden">
-        <!-- Faded Background Image -->
-        <div class="absolute inset-0 z-0 opacity-15 bg-center bg-cover"
-            style="background-image: url('<?php echo get_template_directory_uri(); ?>/assets/images/bg-fenica.jpg');">
-        </div>
+    <?php if (is_front_page() || is_page_template('front-page.php')): ?>
+        <div id="preloader"
+            class="fixed inset-0 z-[9999] bg-[#0e1e2e] flex flex-col items-center justify-center px-6 overflow-hidden">
+            <!-- Faded Background Image -->
+            <div class="absolute inset-0 z-0 opacity-15 bg-center bg-cover"
+                style="background-image: url('<?php echo get_template_directory_uri(); ?>/assets/images/bg-fenica.jpg');">
+            </div>
 
-        <svg class="relative z-10 h-8 sm:h-10 md:h-14 max-w-[85vw] w-auto mb-6 md:mb-8 preloader-logo"
-            id="fenica-logo-svg" xmlns="http://www.w3.org/2000/svg" version="1.1" viewBox="0 0 1156.33 208.54">
-            <defs>
-                <style>
-                    .st0 {
-                        fill: transparent;
-                        stroke: #f0e0ca;
-                        stroke-width: 3px;
-                        stroke-dasharray: 3000;
-                        stroke-dashoffset: 3000;
-                    }
-                </style>
-            </defs>
-            <polygon class="st0"
-                points="147.09 40.49 147.09 3.71 0 3.71 0 204.03 39.91 204.03 39.91 122.91 140.36 122.91 140.36 86.13 39.91 86.13 39.91 40.49 147.09 40.49" />
-            <polygon class="st0"
-                points="237.94 167.4 237.94 121.33 341.53 121.33 341.53 84.71 237.94 84.71 237.94 40.35 346.54 40.35 346.54 3.71 198.01 3.71 198.01 204.03 347.68 204.03 347.68 167.4 237.94 167.4" />
-            <rect class="st0" x="642.4" y="3.71" width="39.93" height="200.31" />
-            <path class="st0"
-                d="M837.71,169.41c-35.34,0-63.1-25.33-63.25-65.54,0-38.92,26.9-65.53,63.25-65.53,22.31,0,40.49,10.31,51.79,27.62l34.34-21.04c-16.31-28.03-48.93-44.92-85.85-44.92-61.67,0-105.17,43.64-105.17,103.87s43.5,103.88,105.17,103.88c36.63,0,66.83-16.31,84.57-42.35l-32.63-23.47c-12.02,17.47-30.05,27.48-52.22,27.48Z" />
-            <path class="st0"
-                d="M1077.93,3.71h-48.66l-77.97,200.31h41.64l17.45-46.64h85.42l17.45,46.64h42.64L1077.93,3.71ZM1024.13,120.62l29.04-77.41,28.9,77.41h-57.94Z" />
-            <path class="st0"
-                d="M555.3,2.61h-11.41c-.73,0-1.32.59-1.32,1.32v82.92c-.02.97-1.08,1.56-1.91,1.05L404.38,3.93c-.88-.44-1.32,0-1.32.88v25.88c0,9.21,5.7,19.3,14.04,24.57l161.36,95.87c.83.49,1.88-.11,1.88-1.07l-.04-119.37c0-16.67-10.53-28.08-25.01-28.08ZM430.7,203.55h10.6c.69,0,1.24-.56,1.24-1.24v-75.53c0-1.32.88-1.75,2.63-.88l132.93,82.48c1.32.44,2.19,0,2.19-1.32v-25.45c0-10.09-5.7-20.18-13.6-24.57l-161.76-95.31c-.83-.49-1.88.11-1.88,1.07v112.66c0,16.67,11.41,28.08,27.64,28.08Z" />
-        </svg>
-        <div class="relative z-10 w-[80vw] max-w-[12rem] md:max-w-[16rem] h-[2px] bg-white/20 rounded overflow-hidden">
-            <div class="h-full bg-[#d4ae6f] w-0 preloader-progress"></div>
+            <svg class="relative z-10 h-8 sm:h-10 md:h-14 max-w-[85vw] w-auto mb-6 md:mb-8 preloader-logo"
+                id="fenica-logo-svg" xmlns="http://www.w3.org/2000/svg" version="1.1" viewBox="0 0 1156.33 208.54">
+                <defs>
+                    <style>
+                        .st0 {
+                            fill: transparent;
+                            stroke: #f0e0ca;
+                            stroke-width: 3px;
+                            stroke-dasharray: 3000;
+                            stroke-dashoffset: 3000;
+                        }
+                    </style>
+                </defs>
+                <polygon class="st0"
+                    points="147.09 40.49 147.09 3.71 0 3.71 0 204.03 39.91 204.03 39.91 122.91 140.36 122.91 140.36 86.13 39.91 86.13 39.91 40.49 147.09 40.49" />
+                <polygon class="st0"
+                    points="237.94 167.4 237.94 121.33 341.53 121.33 341.53 84.71 237.94 84.71 237.94 40.35 346.54 40.35 346.54 3.71 198.01 3.71 198.01 204.03 347.68 204.03 347.68 167.4 237.94 167.4" />
+                <rect class="st0" x="642.4" y="3.71" width="39.93" height="200.31" />
+                <path class="st0"
+                    d="M837.71,169.41c-35.34,0-63.1-25.33-63.25-65.54,0-38.92,26.9-65.53,63.25-65.53,22.31,0,40.49,10.31,51.79,27.62l34.34-21.04c-16.31-28.03-48.93-44.92-85.85-44.92-61.67,0-105.17,43.64-105.17,103.87s43.5,103.88,105.17,103.88c36.63,0,66.83-16.31,84.57-42.35l-32.63-23.47c-12.02,17.47-30.05,27.48-52.22,27.48Z" />
+                <path class="st0"
+                    d="M1077.93,3.71h-48.66l-77.97,200.31h41.64l17.45-46.64h85.42l17.45,46.64h42.64L1077.93,3.71ZM1024.13,120.62l29.04-77.41,28.9,77.41h-57.94Z" />
+                <path class="st0"
+                    d="M555.3,2.61h-11.41c-.73,0-1.32.59-1.32,1.32v82.92c-.02.97-1.08,1.56-1.91,1.05L404.38,3.93c-.88-.44-1.32,0-1.32.88v25.88c0,9.21,5.7,19.3,14.04,24.57l161.36,95.87c.83.49,1.88-.11,1.88-1.07l-.04-119.37c0-16.67-10.53-28.08-25.01-28.08ZM430.7,203.55h10.6c.69,0,1.24-.56,1.24-1.24v-75.53c0-1.32.88-1.75,2.63-.88l132.93,82.48c1.32.44,2.19,0,2.19-1.32v-25.45c0-10.09-5.7-20.18-13.6-24.57l-161.76-95.31c-.83-.49-1.88.11-1.88,1.07v112.66c0,16.67,11.41,28.08,27.64,28.08Z" />
+            </svg>
+            <div class="relative z-10 w-[80vw] max-w-[12rem] md:max-w-[16rem] h-[2px] bg-white/20 rounded overflow-hidden">
+                <div class="h-full bg-[#d4ae6f] w-0 preloader-progress"></div>
+            </div>
         </div>
-    </div>
-    <?php else : ?>
-    <!-- Solid Overlay for Preloading Effect on Subpages -->
-    <div id="preloader"
-        style="position: fixed; top: 0; left: 0; width: 100vw; height: 100vh; z-index: 9999; background-color: #09121d; transition: opacity 0.8s ease-out, visibility 0.8s ease-out; pointer-events: none;">
-        <div id="preloader-canvas" style="width: 100%; height: 100%;"></div>
-    </div>
+    <?php else: ?>
+        <!-- Solid Overlay for Preloading Effect on Subpages -->
+        <div id="preloader"
+            style="position: fixed; top: 0; left: 0; width: 100vw; height: 100vh; z-index: 9999; background-color: #09121d; transition: opacity 0.8s ease-out, visibility 0.8s ease-out; pointer-events: none;">
+            <div id="preloader-canvas" style="width: 100%; height: 100%;"></div>
+        </div>
     <?php endif; ?>
     <!-- Header Section -->
     <header class="relative z-[999]">
@@ -79,9 +79,8 @@
                     </a>
                 </div>
                 <div class="logo-wrap shrink-0">
-                    <img src="https://atsaigonriverside.vn/wp-content/themes/dxmd/assets/images/logos/logo-connect.png"
-                        alt="Logo đối tác" class="logo-img w-20 md:w-32 object-contain transition-all duration-300"
-                        title="Logo đối tác">
+                    <img src="assets/images/logo-cdt-dxmd-fenica.png"src="<?php echo get_template_directory_uri(); ?>/assets/images/logo-cdt-dxmd-fenica.png" alt="Logo đối tác"
+                        class="logo-img w-20 md:w-32 object-contain transition-all duration-300" title="Logo đối tác">
                 </div>
             </div>
 
@@ -89,7 +88,8 @@
             <nav class="border-t border-[#d4ae6f]/20 text-xs">
                 <!-- Desktop Menu -->
                 <ul id="desktop-menu" class="hidden md:flex justify-center space-x-8 relative z-20 text-white">
-                    <div id="magic-line" class="absolute bottom-0 left-0 h-[3px] flex flex-col justify-between transition-all duration-300 pointer-events-none opacity-0 z-[-1]">
+                    <div id="magic-line"
+                        class="absolute bottom-0 left-0 h-[3px] flex flex-col justify-between transition-all duration-300 pointer-events-none opacity-0 z-[-1]">
                         <div class="h-[1px] bg-[#d4ae6f] w-full"></div>
                         <div class="h-[1px] bg-[#d4ae6f] w-full"></div>
                     </div>
@@ -115,7 +115,8 @@
                             class="py-3 px-2 tracking-wider hover:text-[#d4ae6f] transition-all duration-300">TIN
                             TỨC</a></li>
                     <li class="contents"><a href="<?php echo home_url('/faq'); ?>"
-                            class="py-3 px-2 tracking-wider hover:text-[#d4ae6f] transition-all duration-300">FAQ</a></li>
+                            class="py-3 px-2 tracking-wider hover:text-[#d4ae6f] transition-all duration-300">FAQ</a>
+                    </li>
                     <li class="contents"><a href="<?php echo home_url('/lien-he'); ?>"
                             class="py-3 px-2 tracking-wider hover:text-[#d4ae6f] transition-all duration-300">LIÊN
                             HỆ</a></li>
